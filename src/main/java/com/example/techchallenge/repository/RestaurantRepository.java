e44b54cf6cc58153a32fbaad74fb6afd2a964761
Rennan <rennan@ezsolucoes.com.br>
2025-08-04 21:08:52 -0300

    fix: removendo imports

--- src/main/java/com/example/techchallenge/repository/RestaurantRepository.java
+++ src/main/java/com/example/techchallenge/repository/RestaurantRepository.java
@@ -1,14 +1,10 @@
 package com.example.techchallenge.repository;
 
-import com.example.techchallenge.entities.AddressEntity;
 import com.example.techchallenge.entities.RestaurantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
