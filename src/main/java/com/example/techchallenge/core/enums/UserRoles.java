7ea1040764ad4ffda37f1e3c141ffd5231cae467
Rennan <rennan@ezsolucoes.com.br>
2025-09-28 22:20:59 -0300

    fix: Correção do UserRoles

--- src/main/java/com/example/techchallenge/core/enums/UserRoles.java
+++ src/main/java/com/example/techchallenge/core/enums/UserRoles.java
@@ -1,7 +1,18 @@
 package com.example.techchallenge.core.enums;
 
-public enum UserRoles {
-    CLIENTE,
-    ADMIN
+import org.springframework.security.core.GrantedAuthority;
 
+public enum UserRoles implements GrantedAuthority {
+    CLIENTE("user"),
+    ADMIN("admin");
+
+    private final String role;
+
+    UserRoles(String role) {
+        this.role = role;
+    }
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + this.name();
+    }
 }
