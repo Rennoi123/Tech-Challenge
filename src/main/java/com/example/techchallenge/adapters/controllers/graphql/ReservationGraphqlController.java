c3231eb4f2965af5cf14d96be9ba3b8fbd3feaff
Rennan <rennan@ezsolucoes.com.br>
2025-10-07 23:37:55 -0300

    fix: Corrigindo fluxo do graphQL

--- src/main/java/com/example/techchallenge/adapters/controllers/graphql/ReservationGraphqlController.java
+++ src/main/java/com/example/techchallenge/adapters/controllers/graphql/ReservationGraphqlController.java
@@ -5,13 +5,13 @@ import com.example.techchallenge.core.domain.usecase.reservation.CreateReservati
 import com.example.techchallenge.core.domain.usecase.reservation.ListReservationsByRestaurantOwnerUseCase;
 import com.example.techchallenge.core.domain.usecase.reservation.UpdateReservationStatusUseCase;
 import com.example.techchallenge.core.enums.ReservationStatusEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import graphql.GraphQLError;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.execution.ErrorType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
@@ -47,24 +47,31 @@ public class ReservationGraphqlController {
         reservation.setReservationTime(input.reservationTime());
         reservation.setNumberOfPeople(input.numberOfPeople());
         reservation.setStatus(input.status);
-        reservation.setCreatedAt(input.createdAt());
+        reservation.setCreatedAt(LocalDateTime.now());
         reservation.setUpdatedAt(input.updatedAt());
         return createReservation.execute(reservation);
     }
 
     @MutationMapping
-    public Reservation updateReservationStatus(@Argument Long id, @Argument ReservationStatusEnum status) {
-        return updateReservationStatus.execute(id, status);
+    public Reservation updateReservationStatus(@Argument ReservationStatusInput reservationStatusInput) {
+
+        return updateReservationStatus.execute(reservationStatusInput.id, reservationStatusInput.status());
     }
 
     public record ReservationInput(Long id,
                                    Long restaurantId,
                                    Long userId,
+                                   @JsonFormat(pattern="dd/MM/yyyy HH:mm")
                                    LocalDateTime reservationTime,
                                    Integer numberOfPeople,
                                    ReservationStatusEnum status,
                                    LocalDateTime createdAt,
                                    LocalDateTime updatedAt) {}
+
+    public record ReservationStatusInput(
+            Long id,
+            ReservationStatusEnum status
+    ){}
 
 
 
